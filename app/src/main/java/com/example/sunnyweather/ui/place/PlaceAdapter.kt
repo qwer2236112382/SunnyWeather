b7028ea67db34d2cd0ace746ccfb001d235795d1
Lin <lin@LindeMacBook-Pro.local>
2022-02-12 21:28:21 +0800

    four

--- app/src/main/java/com/example/sunnyweather/ui/place/PlaceAdapter.kt
+++ app/src/main/java/com/example/sunnyweather/ui/place/PlaceAdapter.kt
@@ -1,29 +1,46 @@
 package com.example.sunnyweather.ui.place
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.drawerlayout.R
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sunnyweather.logic.model.Place
+import com.example.sunnyweather.ui.weather.WeatherActivity
 
 class PlaceAdapter(private val fragment: Fragment, private val placeList: List<Place>):
     RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
         inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
             val placeName: TextView = view.findViewById(com.example.sunnyweather.R.id.placeName)
             val placeAddress: TextView = view.findViewById(com.example.sunnyweather.R.id.placeAddress)
+            val placeLng :TextView = view.findViewById(com.example.sunnyweather.R.id.placeLng)
+            val placeLat : TextView =view.findViewById(com.example.sunnyweather.R.id.placeLat)
         }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(com.example.sunnyweather.R.layout.place_item,parent, false)
-        return ViewHolder(view)
+        val holder = ViewHolder(view)
+        holder.itemView.setOnClickListener {
+            val position = holder.adapterPosition
+            val place = placeList[position]
+            val intent = Intent(parent.context, WeatherActivity::class.java).apply {
+                putExtra("location_lng", place.location.lng)
+                putExtra("location_lat", place.location.lat)
+                putExtra("place_name", place.name)
+            }
+            fragment.startActivity(intent)
+            //fragment.activity?.finish()
+        }
+        return holder
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val place = placeList[position]
         holder.placeName.text = place.name
         holder.placeAddress.text = place.address
+        holder.placeLng.text = "lon: " + place.location.lng
+        holder.placeLat.text = "lat: " + place.location.lat
     }
 
     override fun getItemCount() = placeList.size
